9d2329f980fc968fc5635aceaf1dab46b72476c7
Romaan Boss <108215028+Romaan313@users.noreply.github.com>
2023-12-24 20:52:44 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,12 +26,16 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT1),
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL1),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_"], user_id=config.OWNER_ID1),
         ],
     ]
     return buttons
